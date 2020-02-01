cbe01aff41efb9350010e7b99370dd27b83cd786
unknown <cristinac.vaillant.com>
2020-02-01 12:31:48 +0100

    otra

--- Program.cs
+++ Program.cs
@@ -18,28 +18,6 @@ namespace iac
 
         AlgoritmoGenetico standard, baldwinian, lamarckian;
 
-        
-        int c=0;
-        string line ;
-        System.IO.StreamReader file =new System.IO.StreamReader(@"D:\archivo.txt");
-        int tamaño=0;
-
-        while((line = file.ReadLine()) != null)  
-       {  
-         
-        var values = (line.Split(' '));
-        tamaño=Int16.Parse(line);
-        System.Console.WriteLine(tamaño); 
-         c++;  
-       }  
-  
-       file.Close();  
-       System.Console.WriteLine("There were {0} lines.", c);  
-       // Suspend the screen.  
-       System.Console.ReadLine(); 
-       
-    
-
         if (todoStandard) {
             Console.WriteLine("*****************************************************************************************");
 
